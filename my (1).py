521a32205ed4dcb0f56ccdccf76833e32ef432e1
Sh1ekh <148334776+Sh1ekh@users.noreply.github.com>
2023-10-18 15:31:46 +0500

    Add files via upload

---
+++ my (1).py
@@ -0,0 +1,17 @@
+A_soni = int(input("Avtomobil sonini kiriting: "))
+Truck_1 = A_soni % 8
+Truck_2 = A_soni / 8 
+if Truck_1 != 0 :
+  print(f"Sizda {Truck_1} ta avto qoldi")
+else:
+  print(f"Sizda {Truck_2} ta Truck kerak")
+
+
+
+
+
+
+
+
+
+
